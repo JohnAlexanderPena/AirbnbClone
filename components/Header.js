da3f95176452ee13c3c70b48e7865b9160164f3b
johnp <jpena91@gmail.com>
2021-11-24 15:54:24 -0500

    add user icons

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/solid";
+import {
+  SearchIcon,
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+} from "@heroicons/react/solid";
 
 const Header = () => {
   return (
@@ -26,7 +31,14 @@ const Header = () => {
       </div>
 
       {/* right */}
-      <div></div>
+      <div className="flex space-x-4 items-center justify-end">
+        <p className="hidden md:inline cursor-pointer">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
     </header>
   );
 };
