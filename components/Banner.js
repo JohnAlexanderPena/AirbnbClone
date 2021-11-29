fd4e5028eb9b91f492ec5e916add54408f2aca9c
johnp <jpena91@gmail.com>
2021-11-29 01:37:14 -0500

    change color style and font size

--- components/Banner.js
+++ components/Banner.js
@@ -10,10 +10,15 @@ const Banner = () => {
         objectFit="cover"
       />
       <div className="absolute top-1/2 w-full text-center">
-        <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+        <p className="text-4xl font-semibold ">
+          Not sure where to go? Perfect.
+        </p>
 
-        <button className="text-purple-500 bg-white px-10 py-4 shadow-md  rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150 ">
-          I'm flexible
+        <button className="bg-white px-10 py-4 shadow-md  rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150 ">
+          <span className="bg-clip-text text-transparent bg-gradient-to-tr from-purple-800 to-pink-600  ">
+            {" "}
+            I'm flexible
+          </span>
         </button>
       </div>
     </div>
